10ff149c93b2d54f3cdea3e96b75a368c991314f
MK Rabbani <133435149+mkError420@users.noreply.github.com>
2025-12-30 21:44:20 +0600

    Final push

---
+++ src/lib/prisma.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "../../generated/prisma/client";
+
+
+const prismaClientSingletion = () => {
+  return new PrismaClient()
+}
+
+declare const globalThis:{
+  prismaGlobal: ReturnType<typeof prismaClientSingletion>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingletion();
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') 
+  globalThis.prismaGlobal = prisma;
+
+
